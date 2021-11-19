ab5a34e100bfe2f23407438cd0c5b61e86363020
YumiMachino <y.y.1515m@gmail.com>
2021-11-18 21:30:07 -0800

    initial commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import MovieListing from '../MovieListing/MovieListing';
+import movieApi from '../../common/apis/movieApi';
+import { APIKey } from '../../common/apis/MovieApiKey';
+import { addMovies } from '../../features/movies/movieSlice';
+import { useDispatch } from 'react-redux';
+
+const Home = () => {
+  const movieText = 'Harry';
+  const dispatch = useDispatch();
+  useEffect(() => {
+    console.log('use effect...');
+
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log('Err: ', err);
+        });
+      dispatch(addMovies(response.data));
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <div className='banner-img'></div>
+      <MovieListing />
+    </div>
+  );
+};
+
+export default Home;
